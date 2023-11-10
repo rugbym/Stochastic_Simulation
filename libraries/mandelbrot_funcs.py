c86c525c0fb87b232191486ae30a5b6234c37aed
rugbym <94176296+rugbym@users.noreply.github.com>
2023-11-10 10:42:00 +0100

    added numba jit speedup of functions

--- libraries/mandelbrot_funcs.py
+++ libraries/mandelbrot_funcs.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from numba import jit
+@jit(nopython=True)
 def mandelbrot(z_n, c):
     """
     returns next iteration of the input mandelbrot number.
@@ -14,7 +15,7 @@ def mandelbrot(z_n, c):
     """
     return z_n**2 + c
 
-
+@jit(nopython=True)
 def mandelbrot_set(c, i_iterations:int=100):
 
     """
@@ -27,14 +28,14 @@ def mandelbrot_set(c, i_iterations:int=100):
         bool: whether the number belongs to the mandelbrotset
     """
     z_n = 0
-    for i in range(100):
+    for _ in range(100):
         z_n = mandelbrot(z_n, c)
         # check if the number is diverging
         if abs(z_n) > 2:
             return False
     return True
 
-
+@jit(nopython=True)
 def mandelbrot_set_image(xmin, xmax, ymin, ymax, width, height, i_iterations:int=100):
     """
     Creates an image of the mandelbrot set.
@@ -54,6 +55,7 @@ def mandelbrot_set_image(xmin, xmax, ymin, ymax, width, height, i_iterations:int
     x = np.linspace(xmin, xmax, width)
     y = np.linspace(ymin, ymax, height)
     image = np.zeros((height, width))
+    
     for i in range(width):
         for j in range(height):
             image[j, i] = mandelbrot_set(complex(x[i], y[j]))
@@ -74,5 +76,6 @@ def plot_mandelbrot_set(xmin, xmax, ymin, ymax, width, height, i_iterations:int=
         i_iterations (int, optional): _description_. Defaults to 100.
     """
     image = mandelbrot_set_image(xmin, xmax, ymin, ymax, width, height)
+    plt.figure(dpi=800)
     plt.imshow(image)
     plt.show()
